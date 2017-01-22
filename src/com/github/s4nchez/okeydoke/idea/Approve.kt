2ddee2a726bff584aa4c2b2adcd70fddeb7c5e60
Ivan Sanchez <s4nchez@gmail.com>
2017-01-22 00:38:28 +0000

    Add different scopes for approval (package/class/method)

--- src/com/github/s4nchez/okeydoke/idea/Approve.kt
+++ src/com/github/s4nchez/okeydoke/idea/Approve.kt
@@ -1,40 +1,78 @@
 package com.github.s4nchez.okeydoke.idea
 
 import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.ConfigurationContext.getFromContext
+import com.intellij.execution.configurations.ConfigurationTypeUtil.findConfigurationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ModuleRootManager
-import com.intellij.openapi.vfs.VfsUtilCore.iterateChildrenRecursively
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.*
+import com.intellij.psi.search.FilenameIndex
 
 class Approve : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        e.module().contentRoots().forEach { root ->
-            iterateChildrenRecursively(root, null, { file ->
-                if (file.isApprovalTestFile()) {
-                    runWriteAction { file.approve() }
-                }
-                true
-            })
-        }
+        val context = getFromContext(e.dataContext)
+        context.psiElement().findApprovalTests().forEach { file -> file.actual?.approve() }
     }
 
     override fun update(e: AnActionEvent) {
-        e.presentation.text = "Approve all '${e.module().name}' tests"
+        e.presentation.text = "Approve Tests"
+        val context = getFromContext(e.dataContext)
+        e.presentation.isVisible = context.isJUnit()
+        e.presentation.isEnabled = context.psiElement().findApprovalTests()
+            .filter { t -> t.actual != null }
+            .isNotEmpty()
     }
 
-    private fun Module.contentRoots() = ModuleRootManager.getInstance(this).contentRoots
+    fun PsiElement?.findApprovalTests(): Collection<ApprovalTest> {
+        return when (this) {
+            is PsiDirectory -> {
+                val psiPackage = JavaDirectoryService.getInstance().getPackage(this)
+                if (psiPackage != null) {
+                    return project.findApprovalTests { f -> f.path.contains(psiPackage.qualifiedName.replace(".", "/")) }
+                }
+                listOf()
+            }
+            is PsiClass -> {
+                return project.findApprovalTests { file -> file.path.contains(pathPrefix()) }
+            }
+            is PsiMethod -> {
+                return project.findApprovalTests { file -> file.path.contains(containingClass?.pathPrefix() + "." + name) }
+            }
+            is PsiIdentifier -> {
+                context.findApprovalTests()
+            }
+            else -> listOf()
+        }
+    }
 
-    private fun AnActionEvent.module() = ConfigurationContext.getFromContext(dataContext).module
-
-    private fun VirtualFile.isApprovalTestFile() = extension == "actual"
+    private fun ConfigurationContext.psiElement() = location?.psiElement
 
     private fun VirtualFile.approve() {
-        val approvalTestFileName = name.replace(Regex("actual$"), "approved")
-        parent.findChild(approvalTestFileName)?.delete(this@Approve)
-        rename(this@Approve, approvalTestFileName)
+        runWriteAction {
+            val approvalTestFileName = name.replace(Regex("actual$"), "approved")
+            parent.findChild(approvalTestFileName)?.delete(this@Approve)
+            rename(this@Approve, approvalTestFileName)
+        }
     }
+
+    private fun ConfigurationContext.isJUnit(): Boolean {
+        val runnerConfig = configuration
+        return runnerConfig != null && runnerConfig.type == findConfigurationType("JUnit")
+    }
+
+    private fun PsiClass.pathPrefix(): String = StringUtil.getPackageName(qualifiedName ?: "").replace(".", "/") + "/" + name
+
+    private fun Project.findApprovalTests(filter: (VirtualFile) -> Boolean): Collection<ApprovalTest> =
+        FilenameIndex.getAllFilesByExt(this, "approved")
+            .filter(filter)
+            .map { ApprovalTest(it, it.dotActualFile()) }
+
+    private fun VirtualFile.dotActualFile(): VirtualFile? = parent.children.find { it.name == name.replace(Regex("approved$"), "actual") }
 }
+
+data class ApprovalTest(val approved: VirtualFile, val actual: VirtualFile?)
