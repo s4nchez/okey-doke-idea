97b28a6eb9b60406fed738b6372099d34fdab43b
Dmitry Kandalov <dmitry.kandalov@springernature.com>
2017-09-21 08:52:53 +0100

    extracted PsiUtil; cleanup

--- src/com/github/s4nchez/okeydoke/idea/Approve.kt
+++ src/com/github/s4nchez/okeydoke/idea/Approve.kt
@@ -3,32 +3,20 @@ package com.github.s4nchez.okeydoke.idea
 import com.intellij.execution.actions.ConfigurationContext
 import com.intellij.execution.actions.ConfigurationContext.getFromContext
 import com.intellij.execution.configurations.ConfigurationTypeUtil.findConfigurationType
-import com.intellij.execution.junit.JUnitUtil
-import com.intellij.execution.junit.JavaRuntimeConfigurationProducerBase
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.impl.status.StatusBarUtil
-import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.FilenameIndex
-import com.intellij.psi.util.PsiTreeUtil.getParentOfType
-import org.jetbrains.kotlin.asJava.toLightClass
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.KtFunction
-import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 
 class Approve : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
         val context = event.configContext
-        val pendingTests = context.psiElement().findTestsPendingApproval()
+        val pendingTests = findTestsPendingApprovalAt(context.psiElement())
         pendingTests.forEach { file -> file.actual?.approve() }
         updateStatusBar(context, pendingTests)
     }
@@ -38,58 +26,31 @@ class Approve : AnAction() {
             val context = event.configContext
             text = "Approve Tests"
             isVisible = context.isJUnit()
-            isEnabled = context.psiElement().findTestsPendingApproval().isNotEmpty()
+            isEnabled = findTestsPendingApprovalAt(context.psiElement()).isNotEmpty()
         }
     }
 
-    private fun PsiElement?.findTestsPendingApproval() = findApprovalTests().filter { it.actual != null }
+    private fun findTestsPendingApprovalAt(psiElement: PsiElement?) = findApprovalTestsAt(psiElement).filter { it.actual != null }
 
-    private fun PsiElement?.findApprovalTests(): Collection<ApprovalTest> {
-        if (this == null) return emptyList()
+    private fun findApprovalTestsAt(psiElement: PsiElement?): Collection<ApprovalTest> {
+        if (psiElement == null) return emptyList()
 
-        val selectedMethod = getTestMethod()
-        if (selectedMethod != null) {
-            return project.findApprovalTests { file -> file.path.contains(selectedMethod.containingClass?.pathPrefix() + "." + selectedMethod.name) }
+        val psiMethod = psiElement.currentTestMethod()
+        if (psiMethod != null) {
+            return psiElement.project.findApprovalTestsAt { file -> file.path.contains(psiMethod.containingClass?.pathPrefix() + "." + psiMethod.name) }
         }
 
-        val selectedClass = getTestClass()
-        if (selectedClass != null) {
-            return project.findApprovalTests { file -> file.path.contains(selectedClass.pathPrefix()) }
+        val psiClass = psiElement.currentTestClass()
+        if (psiClass != null) {
+            return psiElement.project.findApprovalTestsAt { file -> file.path.contains(psiClass.pathPrefix()) }
         }
 
-        val selectedPackage = JavaRuntimeConfigurationProducerBase.checkPackage(this)
-        if (selectedPackage != null) {
-            return project.findApprovalTests { file -> file.path.contains(selectedPackage.qualifiedName.replace(".", "/")) }
+        val psiPackage = psiElement.currentPackage()
+        if (psiPackage != null) {
+            return psiElement.project.findApprovalTestsAt { file -> file.path.contains(psiPackage.qualifiedName.replace(".", "/")) }
         }
 
         return emptyList()
-    }
-
-    private fun PsiElement.getTestMethod(): PsiMethod? {
-        val javaMethod = JUnitUtil.getTestMethod(this)
-        if (javaMethod != null) return javaMethod
-
-        val ktFunction = this.getParentOfType<KtNamedFunction>(false) ?: return null
-        val owner = getParentOfType(ktFunction, KtFunction::class.java, KtClass::class.java)
-
-        if (owner is KtClass) {
-            val delegate = owner.toLightClass() ?: return null
-            val ktMethod = delegate.methods.firstOrNull { it.navigationElement == ktFunction } ?: return null
-            if (JUnitUtil.getTestMethod(ktMethod) != null) return ktMethod
-        }
-        return null
-    }
-
-    private fun PsiElement.getTestClass(): PsiClass? {
-        val javaClass = JUnitUtil.getTestClass(this)
-        if (javaClass != null) return javaClass
-
-        val containingFile = containingFile as? KtFile ?: return null
-        var ktClass = getParentOfType<KtClass>(false)
-        if (!ktClass.isJUnitTestClass()) {
-            ktClass = getTestClassInFile(containingFile)
-        }
-        return ktClass?.toLightClass()
     }
 
     private fun ConfigurationContext.psiElement() = location?.psiElement
@@ -113,20 +74,12 @@ class Approve : AnAction() {
         return runnerConfig != null && runnerConfig.type == findConfigurationType("JUnit")
     }
 
-    private fun PsiClass.pathPrefix(): String = StringUtil.getPackageName(qualifiedName ?: "").replace(".", "/") + "/" + name
-
-    private fun Project.findApprovalTests(filter: (VirtualFile) -> Boolean): Collection<ApprovalTest> =
+    private fun Project.findApprovalTestsAt(filter: (VirtualFile) -> Boolean): Collection<ApprovalTest> =
         FilenameIndex.getAllFilesByExt(this, "approved")
             .filter(filter)
             .map { ApprovalTest(it, it.dotActualFile()) }
 
     private fun VirtualFile.dotActualFile(): VirtualFile? = parent.children.find { it.name == name.replace(Regex("approved$"), "actual") }
-
-    private fun KtClass?.isJUnitTestClass() =
-        this?.toLightClass()?.let { JUnitUtil.isTestClass(it, false, true) } ?: false
-
-    private fun getTestClassInFile(ktFile: KtFile) =
-        ktFile.declarations.filterIsInstance<KtClass>().singleOrNull { it.isJUnitTestClass() }
 
     private val AnActionEvent.configContext: ConfigurationContext get() = getFromContext(this.dataContext)
 
---
+++ src/com/github/s4nchez/okeydoke/idea/PsiUtil.kt
@@ -0,0 +1,53 @@
+package com.github.s4nchez.okeydoke.idea
+
+import com.intellij.execution.junit.JUnitUtil
+import com.intellij.execution.junit.JavaRuntimeConfigurationProducerBase
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.asJava.toLightClass
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+
+fun PsiElement.currentPackage() =
+    JavaRuntimeConfigurationProducerBase.checkPackage(this)
+
+fun PsiElement.currentTestMethod(): PsiMethod? {
+    val javaMethod = JUnitUtil.getTestMethod(this)
+    if (javaMethod != null) return javaMethod
+
+    val ktFunction = this.getParentOfType<KtNamedFunction>(false) ?: return null
+    val owner = PsiTreeUtil.getParentOfType(ktFunction, KtFunction::class.java, KtClass::class.java)
+
+    if (owner is KtClass) {
+        val delegate = owner.toLightClass() ?: return null
+        val ktMethod = delegate.methods.firstOrNull { it.navigationElement == ktFunction } ?: return null
+        if (JUnitUtil.getTestMethod(ktMethod) != null) return ktMethod
+    }
+    return null
+}
+
+fun PsiElement.currentTestClass(): PsiClass? {
+    val javaClass = JUnitUtil.getTestClass(this)
+    if (javaClass != null) return javaClass
+
+    val containingFile = containingFile as? KtFile ?: return null
+    var ktClass = getParentOfType<KtClass>(false)
+    if (!ktClass.isJUnitTestClass()) {
+        ktClass = getTestClassInFile(containingFile)
+    }
+    return ktClass?.toLightClass()
+}
+
+fun PsiClass.pathPrefix(): String = StringUtil.getPackageName(qualifiedName ?: "").replace(".", "/") + "/" + name
+
+private fun KtClass?.isJUnitTestClass() =
+    this?.toLightClass()?.let { JUnitUtil.isTestClass(it, false, true) } ?: false
+
+private fun getTestClassInFile(ktFile: KtFile) =
+    ktFile.declarations.filterIsInstance<KtClass>().singleOrNull { it.isJUnitTestClass() }
