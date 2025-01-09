d24a0534a3321c7c5d23c5a4d63bf58d267a2a19
Ivan Sanchez <ivan@gourame.com>
2025-01-09 09:05:50 +0000

    Use intellij platform plugin

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import java.net.URI
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.intellij.platform") version "2.0.1"
+    id("dev.bmac.intellij.plugin-uploader") version "1.3.3"
+}
+
+repositories {
+    mavenCentral()
+    maven { url = URI("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+val pluginVersion = "0.7.0"
+
+intellijPlatform {
+    pluginConfiguration {
+        version = pluginVersion
+    }
+}
+
+tasks {
+    patchPluginXml {
+        sinceBuild.set("231")
+        version = pluginVersion
+    }
+    publishPlugin {
+        token.set(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2023.1.7")
+        instrumentationTools()
+    }
+}
